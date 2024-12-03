8ea06586a90f4b0e07c829b0a07f1f5d3ec65a66
miguel_borges <miguelborgess2016@gmail.com>
2024-12-03 11:22:16 -0300

    .

--- atividade10.py
+++ atividade10.py
@@ -1,3 +1,43 @@
 # Matheus Villar n° 21 
 # Miguel Borges n° 22 
 # Pedro Zocatelli n° 27 
+
+def temp_ordem (dia1, dia2, dia3, dia4, dia5, dia6, dia7 ):
+    lista = {dia1, dia2, dia3, dia4, dia5, dia6, dia7}
+    numerosordenados = sorted(lista)
+
+    # Dados para o gráfico
+    categorias = ["segunda", "tersa", "quarta", "quinta", "sesta", "sabado", "domingo"]
+    
+    
+    # Criar o gráfico de colunas
+    plt.bar(categorias, numerosordenados, color='skyblue', edgecolor='black')
+    
+    # Títulos e rótulos
+    plt.title("Exemplo de Gráfico de Colunas", fontsize=16)
+    plt.xlabel("Categorias", fontsize=12)
+    plt.ylabel("numerosordenados", fontsize=12)
+    
+    # Adicionar valores no topo das colunas
+    for i, valor in enumerate(numerosordenados):
+        plt.text(i, valor + 1, str(valor), ha='center', fontsize=10)
+    
+    # Mostrar o gráfico
+    return plt.show()
+    
+
+print("insira o dia da semana e a tenperatua que teve em cada dia a:")
+
+segunda = float(input("qual foi a tenperatura da segunda:"))
+terça = float(input("qual foi a tenperatura da terça:"))
+quarta = float(input("qual foi a tenperatura da quarta:"))
+quinta = float(input("qual foi a tenperatura da quinta:"))
+sesta = float(input("qual foi a tenperatura da sesta:"))
+sabado = float(input("qual foi a tenperatura do sabado:"))
+domingo = float(input("qual foi a tenperatura do domingo:"))
+
+temp_ordem (segunda, terça, quarta, quinta, sesta, sabado, domingo  )
+ 
+
+
+    
--- atividade4.py
+++ atividade4.py
@@ -1,3 +1,17 @@
 # Matheus Villar n° 21 
 # Miguel Borges n° 22 
 # Pedro Zocatelli n° 27 
+
+
+Celsius = float(input("digite a tenperatura que você se encontra:"))
+numero = 3
+while (numero != 2 and numero != 1):
+    numero = int(input(f"voce quer converter essa demperatura de {Celsius}° para?  \n digite 1 para Fahrenheit. \n digite 2 para Kelvin:\n"))
+    if (numero == 1 ):
+        Fahrenheit = Celsius * 1.8 + 32
+        print(f"A tenperatura de {Celsius}° em graus celsius em Fahrenheit é de: {Fahrenheit}.")
+    elif (numero == 2):
+        Kelvin = Celsius + 273.15 
+        print(f"A tenperatura de {Celsius}° em graus celsius em Kelvin é de: {Kelvin}.")
+    else:
+        print(f"A opeção de numero: {numero} não é valida tente de novo.")
