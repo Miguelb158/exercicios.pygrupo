2490e03fe0e4adc2fe9083adf84310e0d1709691
miguel_borges <miguelborgess2016@gmail.com>
2024-12-09 09:26:02 -0300

    quassela

--- atividade8.py
+++ atividade8.py
@@ -8,95 +8,27 @@ def somar_elementos (lista):
         somar += numero
     return somar
 
-def media (dias, temp):
-    media = temp / dias
-    return media
+def media(dias, temp):
+    if dias == 0:
+        return 0
+    return temp / dias
             
 with open('arquivo.csv', 'r') as file:
     reader = csv.reader(file)
 
 
-    Janeiro = []
-    somar_Janeiro = []
-    dias_Janeiro = []
-    media_Janeiro = media(dias_Janeiro,somar_Janeiro)
-
-# ===============================================
-
-    fevereiro = []
-    somar_fevereiro = []
-    dias_fevereiro = []
-    media_fevereiro = media(dias_fevereiro,somar_fevereiro)
-
-# ===============================================
-
-    Março = []
-    somar_Março = []
-    dias_Março = []
-    media_Março = media(dias_Março,somar_Março)
-    
-# ===============================================
-
-    Abril = []
-    somar_Abril = []
-    dias_Abril  = []
-    media_Abril = media(dias_Abril,somar_Abril)
-
-# ===============================================
-
-    Maio = []
-    somar_Maio = []
-    dias_Maio = []
-    media_Maio = media(dias_Maio,somar_Maio)
-    
-# ===============================================
-
-    Junho = []
-    somar_Junho = []
-    dias_Junho = []
-    media_Junho = media(dias_Junho,somar_Junho)
-
-# ===============================================
-
-    Julho = []
-    somar_Julho = []
-    dias_Julho = []
-    media_Julho = media(dias_Julho,somar_Julho)
-
-# ===============================================
-
-    Agosto = []
-    somar_Agosto = []
-    dias_Agosto = []
-    media_Agosto = media(dias_Agosto,somar_Agosto)
-    
-# ===============================================
-
-    Setembro = []
-    somar_Setembro = []
-    dias_Setembro = []
-    media_Setembro = media(dias_Setembro,somar_Setembro)
-
-# ===============================================
-
-    Outubro = []
-    somar_Outubro = []
-    dias_Outubro = []
-    media_Outubro = media(dias_Outubro,somar_Outubro)
-
-# ===============================================
-
-    Novembro = []
-    somar_Novembro = []
-    dias_Novembro = []
-    media_Novembro = media(dias_Novembro,somar_Novembro)
-    
-# ===============================================
-
-    Dezembro = []
-    somar_Dezembro = []
-    dias_Dezembro = []
-    media_Dezembro = media(dias_Dezembro,somar_Dezembro)
+    somar_Janeiro, dias_Janeiro = 0, 0
+    somar_fevereiro, dias_fevereiro = 0, 0
+    somar_Março, dias_Março = 0, 0
+    somar_Abril, dias_Abril = 0, 0
+    somar_Maio, dias_Maio = 0, 0
+    somar_Junho, dias_Junho = 0, 0
+    somar_Julho, dias_Julho = 0, 0
+    somar_Agosto, dias_Agosto = 0, 0
+    somar_Setembro, dias_Setembro = 0, 0
+    somar_Outubro, dias_Outubro = 0, 0
+    somar_Novembro, dias_Novembro = 0, 0
+    somar_Dezembro, dias_Dezembro = 0, 0
 
 
 
@@ -107,88 +39,101 @@ with open('arquivo.csv', 'r') as file:
 
     for n in reader:
         if n[0] == "Janeiro":
-            Janeiro.append(float(n[2]))
-            somar_Janeiro =(somar_elementos(Janeiro))
-            dias_Janeiro = len(Janeiro)
+            somar_Janeiro += float(n[2])
+            dias_Janeiro += 1
 
             # ========================================
 
-        elif n[0] == "fevereiro":
-            fevereiro.append(float(n[2]))
-            somar_fevereiro =(somar_elementos(fevereiro))
-            dias_fevereiro = len(fevereiro)
+        elif n[0] == "Fevereiro":
+            somar_fevereiro += float(n[2])
+            dias_fevereiro += 1
 
              # ========================================
 
         elif n[0] == "Março":
-            Março.append(float(n[2]))
-            somar_Março =(somar_elementos(Março))
-            dias_Março = len(Março)
+            print (n)
+            somar_Março += float(n[2])
+            dias_Março += 1
 
              # ========================================
 
         elif n[0] == "Abril":
-            Abril.append(float(n[2]))
-            somar_Abril =(somar_elementos(Abril))
-            dias_Abril = len(Abril)
+            somar_Abril += float(n[2])
+            dias_Abril += 1
 
              # ========================================
              
         elif n[0] == "Maio":
-            Maio.append(float(n[2]))
-            somar_Maio =(somar_elementos(Maio))
-            dias_Maio = len(Maio)
+            somar_Maio += float(n[2])
+            dias_Maio += 1
 
             # ========================================
 
         elif n[0] == "Junho":
-            Junho.append(float(n[2]))
-            somar_Junho =(somar_elementos(Junho))
-            dias_Junho = len(Junho)
+            somar_Junho += float(n[2])
+            dias_Junho += 1
 
              # ========================================
 
         elif n[0] == "Julho":
-            Julho.append(float(n[2]))
-            somar_Julho =(somar_elementos(Julho))
-            dias_Julho = len(Julho)
+            somar_Julho += float(n[2])
+            dias_Julho += 1
 
              # ========================================
 
         elif n[0] == "Agosto":
-            Agosto.append(float(n[2]))
-            somar_Agosto =(somar_elementos(Agosto))
-            dias_Agosto = len(Agosto)
+            somar_Agosto += float(n[2])
+            dias_Agosto += 1
 
              # ========================================
              
         elif n[0] == "Setembro":
-            Setembro.append(float(n[2]))
-            somar_Setembro =(somar_elementos(Setembro))
-            dias_Setembro = len(Setembro)
+            somar_Setembro += float(n[2])
+            dias_Setembro += 1
 
                          # ========================================
 
         elif n[0] == "Outubro":
-            Outubro.append(float(n[2]))
-            somar_Outubro =(somar_elementos(Outubro))
-            dias_Outubro = len(Outubro)
+            somar_Outubro += float(n[2])
+            dias_Outubro += 1
 
              # ========================================
 
         elif n[0] == "Novembro":
-            Novembro.append(float(n[2]))
-            somar_Novembro =(somar_elementos(Novembro))
-            dias_Novembro = len(Novembro)
+            somar_Novembro += float(n[2])
+            dias_Novembro += 1
 
              # ========================================
              
         elif n[0] == "Dezembro":
-            Dezembro.append(float(n[2]))
-            somar_Dezembro =(somar_elementos(Dezembro))
-            dias_Dezembro = len(Dezembro)
+            somar_Dezembro += float(n[2])
+            dias_Dezembro += 1
 
-    print (f"A temperatura media de cada mees durante esse ano é: /n========= /n Janeiro: {media_Janeiro} /n fevereiro: {media_fevereiro}/n Março: {media_Março} Abril: {media_Abril} /n Maio: {media_Maio}/n Junho: {media_Junho} /n Julho: {media_Julho}/n Agosto: {media_Agosto} Setembro: {media_Setembro} /n Outubro: {media_Outubro}/n Novembro: {media_Novembro}/n Dezembro: {media_Dezembro} ")
+    media_Janeiro = media(dias_Janeiro,somar_Janeiro)
+
+    media_fevereiro = media(dias_fevereiro,somar_fevereiro)
+
+    media_Março = media(dias_Março,somar_Março)
+
+    media_Abril = media(dias_Abril,somar_Abril)
+
+    media_Maio = media(dias_Maio,somar_Maio)
+
+    media_Junho = media(dias_Junho,somar_Junho)
+
+    media_Julho = media(dias_Julho,somar_Julho)
+
+    media_Agosto = media(dias_Agosto,somar_Agosto)
+
+    media_Setembro = media(dias_Setembro,somar_Setembro)
+
+    media_Outubro = media(dias_Outubro,somar_Outubro)
+
+    media_Novembro = media(dias_Novembro,somar_Novembro)
+
+    media_Dezembro = media(dias_Dezembro,somar_Dezembro)
+
+    print (f"A temperatura media de cada mees durante esse ano é: \n =======================\n Janeiro: {media_Janeiro} \n fevereiro: {media_fevereiro}\n Março: {media_Março}\n Abril: {media_Abril} \n Maio: {media_Maio}\n Junho: {media_Junho} \n Julho: {media_Julho}\n Agosto: {media_Agosto}\n Setembro: {media_Setembro} \n Outubro: {media_Outubro}\n Novembro: {media_Novembro}\n Dezembro: {media_Dezembro} ")
 
 
  
